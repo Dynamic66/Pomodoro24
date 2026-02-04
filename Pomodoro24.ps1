0b50235799913bd15680f7cf941c0dd5b907cb82
Dynamic66 <160179474+Dynamic66@users.noreply.github.com>
2026-02-04 23:31:49 +0100

    Refactor Pomodoro Timer script for clarity and usability
    Overall performance improvement

--- Pomodoro24.ps1
+++ Pomodoro24.ps1
@@ -15,12 +15,23 @@
     - To start the timer, simply click on the PowerShell window. The timer will begin counting down from the default 30-minute interval.
     - To pause the timer, click on the PowerShell window again. The timer will freeze at its current value.
     - To adjust the timer length, right-click on the PowerShell window to open the context menu. Select 'Set Timer Length' and enter a new value in minutes.
+    - To reset the timer and Pomodoro count, select 'Reset' from the context menu.
+    - To open multiple timers, select 'Open another timer' from the context menu.
+    - To exit the application, select 'Exit' from the context menu.
+    - The timer will play an alarm sound (from a specified .wav file) when the countdown reaches zero.
+    - The timer window can be set to 'Topmost' mode, keeping it above other windows for easy visibility.
+    - The timer window can be moved by clicking and dragging it to a new location on the screen.
+    - A progress bar at the bottom of the window visually indicates the remaining time in the current Pomodoro interval.
+    - The current Pomodoro count is displayed at the bottom of the window, showing how many Pomodoros have been completed out of a set maximum (default is 4).
 
 .PARAMETER TimerLength (OPTIONAL)
     Specifies the length of the timer in seconds. The default value is 30 seconds.
 
 .PARAMETER AlarmFilePath (OPTIONAL)
     Specifies the file path of the .wav file to play as the alarm sound. The file path must end with the .wav extension. The default value is C:\Windows\Media\Alarm03.wav.
+
+.PARAMETER FormBorderStyle (OPTIONAL) 
+    Specifies the border style of the timer window. Valid options are 'Sizable', 'None', and 'FixedSingle'. The default value is 'FixedSingle'.
 
 .NOTES
     Benefits of the Pomodoro Technique
@@ -34,276 +45,254 @@
 
 
 function Show-Timer {
-param(
-    [int]$TimerLength = 30,
-    [ValidatePattern('^[a-zA-Z0-9_\-]+\.wav$')] # pass .wav files only
-    [string]$AlarmFilePath = 'C:\Windows\Media\Alarm03.wav',
-    [Validatepattern('^([Ss]izable|[Nn]one|[Ff]ixed[Ss]ingle)$')]
-    $FormBorderStyle = 'FixedSingle'
-)
-#region Declaration
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-[System.Windows.Forms.Application]::EnableVisualStyles()
+    param(
+        [int]$TimerLength = 30,
+        [ValidatePattern('\.wav$')]
+        [string]$AlarmFilePath = 'C:\Windows\Media\Alarm03.wav',
+        [Validatepattern('^([Ss]izable|[Nn]one|[Ff]ixed[Ss]ingle)$')]
+        $FormBorderStyle = 'FixedSingle'
+    )
+    #region Declaration
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+    [System.Windows.Forms.Application]::EnableVisualStyles()
 
-$alarm = New-Object System.Media.SoundPlayer($AlarmFilePath)
+    $alarm = New-Object System.Media.SoundPlayer($AlarmFilePath)
 
-$fUI = New-Object system.windows.forms.form
-$lDisplay = New-Object System.Windows.Forms.Label
-$lPomodoros = New-Object System.Windows.Forms.Label
-$lState = New-Object System.Windows.Forms.Label
-$pProgress = New-Object System.Windows.Forms.ProgressBar
-#$pSec = New-Object System.Windows.Forms.ProgressBar
+    $fUI = New-Object system.windows.forms.form
+    $lDisplay = New-Object System.Windows.Forms.Label
+    $lPomodoros = New-Object System.Windows.Forms.Label
+    $lState = New-Object System.Windows.Forms.Label
+    $pProgress = New-Object System.Windows.Forms.ProgressBar
 
-$tUIUpdater = New-Object System.Windows.forms.timer 
-$tdelay = New-Object System.Windows.Forms.Timer
+    $tUIUpdater = New-Object System.Windows.forms.timer 
+    $tdelay = New-Object System.Windows.Forms.Timer
 
-$Contextmenue = New-Object System.Windows.Forms.ContextMenuStrip
-$lMin = New-Object System.Windows.Forms.ToolStripLabel
-$tMin = New-Object System.Windows.Forms.ToolStripTextBox
-$bExit = New-Object System.Windows.Forms.ToolStripButton
-$bReset = New-Object System.Windows.Forms.ToolStripButton
-$bNewTimer = New-Object System.Windows.Forms.ToolStripButton
-$bTopmost = New-Object System.Windows.Forms.ToolStripButton
-$bTimePicker = New-Object System.Windows.Forms.ToolStripButton
+    $Contextmenue = New-Object System.Windows.Forms.ContextMenuStrip
+    $lMin = New-Object System.Windows.Forms.ToolStripLabel
+    $tMin = New-Object System.Windows.Forms.ToolStripTextBox
+    $bExit = New-Object System.Windows.Forms.ToolStripButton
+    $bReset = New-Object System.Windows.Forms.ToolStripButton
+    $bNewTimer = New-Object System.Windows.Forms.ToolStripButton
+    $bTopmost = New-Object System.Windows.Forms.ToolStripButton
 
-$script:state = '▶'
-$alarm.LoadAsync()
+    $global:state = '▶'
+    $alarm.LoadAsync()
 
 
-function Update-DisplayString {
+    function Update-DisplayString {
+        $newtimeSpan = [TimeSpan]::FromSeconds($global:counter)
+        [string]$time = $newtimeSpan.TotalSeconds
 
-    #[string]$time = '{0:mm\:ss}' -f (New-TimeSpan -Seconds $counter)  #formats seconds to a 00:00 format
-    #changed it back to total seconds. seams to reduce flickering
-    $newtimeSpan = New-TimeSpan -Seconds $counter
-    [string]$time = $newtimeSpan.TotalSeconds
+        $lDisplay.Text = [string]$time
 
-    #(Measure-Command {
-    $lDisplay.Text = [string]$time
-    #}).ticks | oh
-
-    $lState.text = $state
-
-    #$psec.Value = 100/60 * $newtimeSpan.Seconds
-    #if($pSec.Value -eq 0){
-    #    $psec.Value = 100
-    #}
+        $lState.text = $global:state
     
-    $pProgress.Value = (100 / (60 * $tMin.Text)) * $Counter 
-    if($pProgress.Value -eq 0){
-        $pProgress.Value = 100
+        $pProgress.Value = (100 / (60 * $tMin.Text)) * $global:counter 
+        if ($pProgress.Value -eq 0) {
+            $pProgress.Value = 100
+        }
     }
+    #endregion
 
-}
-#endregion
-
-#region Contextmenu
-$bTopmost.add_click({
-        if ($fui.topmost) {
-            $fui.topmost = $false
-            $bTopmost.text = '📌  Topmost: Off'
-        }
-        else {
-            $fui.topmost = $true
-            $bTopmost.text = '📌  Topmost: On'
-        }
-    })
-$bTopmost.text = '📌  Topmost: On'
-
-
-$bNewTimer.add_click({
-        Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-file $$"
-    })
-$bNewTimer.Text = '⏲  Open another timer'
-
-$lMin.Text = '⏳   Set timer minutes'
-
-$tmin.add_TextChanged({
-        $tmin.text = $tMin.Text -Replace ('\D')
-        $script:Counter = 60 * $tMin.Text 
-        Update-DisplayString
-    })
-$tMin.Text = $timerLength
-$tMin.MaxLength = 4
-$tMin.Margin = '20,0,0,0'
-$tMin.BackColor = 'whitesmoke'
-
-$bReset.add_click({
-        $script:Counter = 60 * $tMin.Text
-        $script:pomodorocounter = 0
-        $lPomodoros.Text = "$script:pomodoroCounter/$pomodoroMaximum"
-        Update-DisplayString
-    })
-$bReset.text = '↩  Reset'
-$bReset.Dock = 'fill'
-$bReset.ForeColor = 'darkblue'
-$bReset.TextAlign = 'MiddleLeft'
-
-$bExit.add_click({ $fui.Close() })
-$bExit.text = '❌  Exit'
-$bExit.TextAlign = 'MiddleLeft'
-$bExit.ForeColor = 'darkred'
-$bExit.Dock = 'fill'
-
-$Contextmenue.BackColor = 'whitesmoke'
-$Contextmenue.Opacity = 0.95
-$Contextmenue.ShowCheckMargin = $false
-$Contextmenue.ShowImageMargin = $false
-$Contextmenue.ShowItemToolTips = $false
-
-
-#void to supress output from this action
-[void]$Contextmenue.Items.add($lMin)
-[void]$Contextmenue.Items.add($tMin)
-[void]$Contextmenue.Items.add($bTopmost)
-[void]$Contextmenue.Items.add($bNewTimer)
-[void]$Contextmenue.Items.add($bReset)
-[void]$Contextmenue.Items.add($bExit)
-#endregion
-
-#region MoveControl
-$lDisplay.add_mousedown({
-        $script:isMoving = $false
-        $tdelay.Start()
-        $script:initialPosition = $args[1] | Select-Object x, y
-    })
-    
-$lDisplay.add_MouseMove({
-        if ($script:moved) {
-            if ($args.Button -eq [System.Windows.Forms.MouseButtons]::Left) {
-
-                $script:isMoving = $true   
-                $fUI.Cursor = [System.Windows.Forms.Cursors]::SizeAll
-                $currentPos = [System.Windows.Forms.Cursor]::Position
-                #grabs the form at the point when there mouse was clicked and moves it at this point #-5 because the controlbox offset
-                $fUI.Location = [System.Drawing.Point]::new([int]$currentPos.x - $script:initialPosition.x - 5, [int]$currentPos.y - $script:initialPosition.y - 5)  
-                # $fUI.Location = [System.Drawing.Point]::new([int]$currentPos.x - $script:initialPosition.x, [int]$currentPos.y - $script:initialPosition.y)  
-
+    #region Contextmenu
+    $bTopmost.add_click({
+            if ($fui.topmost) {
+                $fui.topmost = $false
+                $bTopmost.text = '📌  Topmost: Off'
             }
-        }
-    })
-
-$lDisplay.add_MouseUp({
-        if ($_.Button -eq [System.Windows.Forms.MouseButtons]::Left) {
-            $script:moved = $false
-            $fUI.Cursor = [System.Windows.Forms.Cursors]::Default
-        }
-    })
-
-$tdelay.Add_Tick({
-        $Script:moved = $true
-        $tdelay.Stop()
-    })
-$tdelay.Interval = 120 
-#endregion
-
-#region UI
-
-$pProgress.dock = 'bottom'
-$pProgress.Size = '20,5' # x does not matter cuz dock
-
-#$pSec.dock = 'bottom'
-#$pSec.Size = '20,3' # x does not matter cuz dock
-
-$lState.Font = 'microsoft Yi Baiti,16' 
-$lState.dock = 'top'
-$lState.TextAlign = 'MiddleCenter'
-$lState.Text = $state
+            else {
+                $fui.topmost = $true
+                $bTopmost.text = '📌  Topmost: On'
+            }
+        })
+    $bTopmost.text = '📌  Topmost: On'
 
 
-$script:pomodoroCounter = 0
-$script:pomodoroMaximum = 4
-$lPomodoros.Text = "$script:pomodoroCounter/$pomodoroMaximum"
-$lPomodoros.dock = 'bottom'
-$lPomodoros.TextAlign = 'topcenter'
+    $bNewTimer.add_click({
+            Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-file $$"
+        })
+    $bNewTimer.Text = '⏲  Open another timer'
+
+    $lMin.Text = '⏳   Set timer minutes'
+
+    $tmin.add_TextChanged({
+            $tmin.text = $tMin.Text -replace ('\D')
+            $global:counter = 60 * $tMin.Text 
+            Update-DisplayString
+        })
+    $tMin.Text = $timerLength
+    $tMin.MaxLength = 4
+    $tMin.Margin = '20,0,0,0'
+    $tMin.BackColor = 'whitesmoke'
+
+    $bReset.add_click({
+            $global:counter = 60 * $tMin.Text
+            $global:pomodoroCounter = 0
+            $lPomodoros.Text = "$global:pomodoroCounter/$global:pomodoroMaximum"
+            Update-DisplayString
+        })
+    $bReset.text = '↩  Reset'
+    $bReset.Dock = 'fill'
+    $bReset.ForeColor = 'darkblue'
+    $bReset.TextAlign = 'MiddleLeft'
+
+    $bExit.add_click({ $fui.Close() })
+    $bExit.text = '❌  Exit'
+    $bExit.TextAlign = 'MiddleLeft'
+    $bExit.ForeColor = 'darkred'
+    $bExit.Dock = 'fill'
+
+    $Contextmenue.BackColor = 'whitesmoke'
+    $Contextmenue.Opacity = 0.95
+    $Contextmenue.ShowCheckMargin = $false
+    $Contextmenue.ShowImageMargin = $false
+    $Contextmenue.ShowItemToolTips = $false
+
+    [void]$Contextmenue.Items.add($lMin)
+    [void]$Contextmenue.Items.add($tMin)
+    [void]$Contextmenue.Items.add($bTopmost)
+    [void]$Contextmenue.Items.add($bNewTimer)
+    [void]$Contextmenue.Items.add($bReset)
+    [void]$Contextmenue.Items.add($bExit)
+    #endregion
+
+    #region MoveControl
+    $lDisplay.add_mousedown({
+            $global:isMoving = $false
+            $tdelay.Start()
+            $global:initialPosition = $args[1] | Select-Object x, y
+        })
+    
+    $lDisplay.add_MouseMove({
+            #moves the window when the mouse is held down
+            if ($global:moved) {
+                if ($args.Button -eq [System.Windows.Forms.MouseButtons]::Left) {
+
+                    $global:isMoving = $true   
+                    $fUI.Cursor = [System.Windows.Forms.Cursors]::SizeAll
+                    $currentPos = [System.Windows.Forms.Cursor]::Position
+                    # -5 because the controlbox offset
+                    $fUI.Location = [System.Drawing.Point]::new([int]$currentPos.x - $global:initialPosition.x - 5, [int]$currentPos.y - $global:initialPosition.y - 5)  
+
+                }
+            }
+        })
+
+    $lDisplay.add_MouseUp({
+            if ($_.Button -eq [System.Windows.Forms.MouseButtons]::Left) {
+                $global:moved = $false
+                $fUI.Cursor = [System.Windows.Forms.Cursors]::Default
+            }
+        })
+
+    $tdelay.Add_Tick({
+            $global:moved = $true
+            $tdelay.Stop()
+        })
+    $tdelay.Interval = 120 
+    #endregion
+
+    #region UI
+    $pProgress.dock = 'bottom'
+    $pProgress.Size = '20,5'
+
+    $lState.Font = 'microsoft Yi Baiti,16' 
+    $lState.dock = 'top'
+    $lState.TextAlign = 'MiddleCenter'
+    $lState.Text = $global:state
 
 
-$lDisplay.add_click({
-        if ($args.button -eq 'left') {     
-            if (-not $script:isMoving) {
+    $global:pomodoroCounter = 0
+    $global:pomodoroMaximum = 4
+    $lPomodoros.Text = "$global:pomodoroCounter/$global:pomodoroMaximum"
+    $lPomodoros.dock = 'bottom'
+    $lPomodoros.TextAlign = 'topcenter'
+
+
+    $lDisplay.add_click({
+            if ($args.button -eq 'left' -and -not $global:isMoving) {     
                 #reset timer
-                if ($script:Counter -le 0) {
-                    $script:Counter = 60 * $tMin.Text
-                    $script:pomodoroCounter++
-                    $lPomodoros.Text = "$script:pomodoroCounter/$pomodoroMaximum"
+                if ($global:counter -le 0) {
+                    $global:counter = 60 * $tMin.Text
+                    $global:pomodoroCounter++
+                    $lPomodoros.Text = "$global:pomodoroCounter/$global:pomodoroMaximum"
                 }
                 #start timer
                 if ($tUIUpdater.Enabled) {
                     $tUIUpdater.Enabled = $false
-                    $script:state = '▶'
+                    $global:state = '▶'
                 }
                 #pause timer
                 else {
                     $tUIUpdater.Enabled = $true
-                    $script:state = '⏸'
+                    $global:state = '⏸'
                     
                 }
                 #Update Timer
                 $fui.BackColor = 'whitesmoke'
                 Update-DisplayString
             }
-        }
-    })
-$lDisplay.dock = 'fill'
-$lDisplay.TextAlign = 'middlecenter'
-$lDisplay.Font = 'microsoft Yi Baiti,16' 
-$lDisplay.ContextMenuStrip = $Contextmenue
-$lDisplay.UseCompatibleTextRendering = $false #better drawstability test
-$lDisplay.UseMnemonic = $false #better drawstability test
-Update-DisplayString #load initial display
+        })
+    $lDisplay.dock = 'fill'
+    $lDisplay.TextAlign = 'middlecenter'
+    $lDisplay.Font = 'microsoft Yi Baiti,16' 
+    $lDisplay.ContextMenuStrip = $Contextmenue
+    $lDisplay.UseCompatibleTextRendering = $false #better drawstability
+    $lDisplay.UseMnemonic = $false #better drawstability
 
-$tUIUpdater.add_tick({  
-        if ($Counter -gt 0) {
-            $script:Counter = $Counter - 1
-        }
-        else {
-            $script:state = '↩'
-            $tUIUpdater.Enabled = $false
-            if (-not $fUI.TopMost) {
-                $fUI.TopMost = $true
-                $fUI.TopMost = $false
+    Update-DisplayString #load initial display
 
+    $tUIUpdater.add_tick({  
+            if ($global:counter -gt 0) {
+                $global:counter = $global:counter - 1
             }
-            $fui.BackColor = 'yellow'
-            & { $alarm.Play() }
-        }
-        Update-DisplayString
-    })
-$tUIUpdater.Interval = 1000
+            else {
+                $global:state = '↩'
+                $tUIUpdater.Enabled = $false
+                if (-not $fUI.TopMost) {
+                    $fUI.TopMost = $true
+                    $fUI.TopMost = $false
 
-$fui.add_shown({
-        $fUI.TopMost = $true
-        #$fUI.TopMost = $false
-    })
-$fUI.BackColor = 'whitesmoke'
-$fUI.ShowIcon = $false
-$fUI.MaximizeBox = $false
-$fUI.ControlBox = $false
-$fUI.Opacity = 0.85
-$fui.FormBorderStyle = $FormBorderStyle 
-$fUI.Size = '140,140'
-$fui.SizeGripStyle = 'show'
-$fUI.AutoScaleMode = 'Font'
-$fUI.StartPosition = 'CenterScreen'
+                }
+                $fui.BackColor = 'yellow'
+                & { $alarm.Play() }
+            }
+            Update-DisplayString
+        })
+    $tUIUpdater.Interval = 1000
 
-$fui.Controls.add($lState)
-$fUI.controls.Add($lDisplay)
+    $fui.add_shown({
+            $fUI.TopMost = $true
+        })
+    $fUI.BackColor = 'whitesmoke'
+    $fUI.ShowIcon = $false
+    $fUI.MaximizeBox = $false
+    $fUI.ControlBox = $false
+    $fUI.Opacity = 0.85
+    $fui.FormBorderStyle = $FormBorderStyle 
+    $fUI.Size = '140,140'
+    $fui.SizeGripStyle = 'show'
+    $fUI.AutoScaleMode = 'Font'
+    $fUI.StartPosition = 'CenterScreen'
 
-$fUI.controls.Add($lPomodoros)
-#$fUI.controls.Add($pSec)
-$fUI.controls.Add($pProgress)
-#endregion
-#region start app
-[System.Windows.Forms.Application]::Run($fUI)
+    $fui.Controls.add($lState)
+    $fUI.controls.Add($lDisplay)
 
-#region cleanup
-# maybe not needed? had problems witht this in onther projects
-$tUIUpdater.Stop() 
-$tUIUpdater.Dispose()
-$tdelay.Stop()
-$tdelay.Dispose()
-$alarm.Dispose()
-#endregion
+    $fUI.controls.Add($lPomodoros)
+    $fUI.controls.Add($pProgress)
+    #endregion
+    #region start app
+    [System.Windows.Forms.Application]::Run($fUI)
+
+    #region cleanup
+    $tUIUpdater.Stop() 
+    $tUIUpdater.Dispose()
+    $tdelay.Stop()
+    $tdelay.Dispose()
+    $alarm.Dispose()
+    #endregion
 }
 
 Show-Timer
+
